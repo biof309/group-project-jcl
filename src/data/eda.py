bda06d83d2b6150efad93d23f7be62bf187c6c60
Cara0 <cara.a.crawford@gmail.com>
2018-12-06 18:56:08 -0500

    add notebook with a plot of the variables

--- src/data/eda.py
+++ src/data/eda.py
@@ -1,3 +1,11 @@
 import pandas as pd
+import seaborn as sns
 
-df = pd.read_csv(file = 'train.csv')
+df = pd.read_csv('train.csv')
+df.head()
+df.info()
+
+df1 = df[['Elevation', 'Aspect', 'Slope']]
+df1.head()
+
+sns.pairplot(df1)
