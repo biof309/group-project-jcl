edc4d1bccea0e724cede59a8bb4f6d95020aa316
Cara0 <cara.a.crawford@gmail.com>
2018-12-06 19:47:44 -0500

    add import plt and plt.show

--- src/data/eda.py
+++ src/data/eda.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import seaborn as sns
-
+import matplotlib.pyplot as plt
 df = pd.read_csv('train.csv')
 df.head()
 df.info()
@@ -9,3 +9,4 @@ df1 = df[['Elevation', 'Aspect', 'Slope']]
 df1.head()
 
 sns.pairplot(df1)
+plt.show()
