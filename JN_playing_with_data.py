924d3c779e830fa499a7fb5af9dded4dc29583f3
Jacob Noeker <jnoek24@outlook.com>
2018-12-08 20:47:50 -0500

    adding more detail and trying different methods for fitting a linear regression model

--- JN_playing_with_data.py
+++ JN_playing_with_data.py
@@ -2,6 +2,8 @@ import pandas as pd
 import sklearn
 df = pd.read_csv('train.csv')
 import numpy as np
+import matplotlib as plt
+
 
 df.head()
 df.info()
@@ -33,7 +35,7 @@ knn.fit(X_train, y_train)
 # Print the accuracy
 print(knn.score(X_test, y_test))
 
-
+# This was interesting to see KNN for cover type and see how the accurate the model was
 
 # Create arrays for features and target variable
 X = df['Elevation']
@@ -49,8 +51,11 @@ X = np.reshape(X, -1, 1)
 
 print("Dimensions of y after reshaping: {}".format(y.shape))
 print("Dimensions of X after reshaping: {}".format(X.shape))
-# I cannot figure out how to reshape this and I am not sure if it is possible because it is (15120, ) and I think
-# needs to be (15120, 1) idk what to do yet. COME BACK TO THIS
+
+''' 
+I cannot figure out how to reshape this and I am not sure if it is possible because it is (15120, ) and I think
+needs to be (15120, 1) idk what to do yet. COME BACK TO THIS
+'''
 
 # Import LinearRegression
 from sklearn.linear_model import LinearRegression
@@ -68,10 +73,48 @@ reg.fit(X, y)
 y_pred = reg.predict(prediction_space)
 
 # Print R^2
-print(reg.score(X_fertility, y))
+print(reg.score(X, y))
 
 # Plot regression line
 plt.plot(prediction_space, y_pred, color='black', linewidth=3)
 plt.show()
 
+'''
+the below script is what we need to do to set hyperparameters and compute metrics of a logistic regression for our data
+the problem is that I dont know how to reshape the data properly so the tests fail at the end
+'''
 
+
+X = df['Cover_Type']
+
+y = df['Elevation']
+
+# Import necessary modules
+from sklearn.linear_model import ElasticNet
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV, train_test_split
+
+
+# Create train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+
+# Create the hyperparameter grid
+l1_space = np.linspace(0, 1, 30)
+param_grid = {'l1_ratio': l1_space}
+
+# Instantiate the ElasticNet regressor: elastic_net
+elastic_net = ElasticNet()
+
+# Setup the GridSearchCV object: gm_cv
+gm_cv = GridSearchCV(elastic_net, param_grid, cv=5)
+
+# Fit it to the training data
+gm_cv.fit(X_train, y_train)
+
+# Predict on the test set and compute metrics
+y_pred = gm_cv.predict(X_test)
+r2 = gm_cv.score(X_test, y_test)
+mse = mean_squared_error(y_test, y_pred)
+print("Tuned ElasticNet l1 ratio: {}".format(gm_cv.best_params_))
+print("Tuned ElasticNet R squared: {}".format(r2))
+print("Tuned ElasticNet MSE: {}".format(mse))
