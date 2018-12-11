3e88fb9ab1c1fe267411e63e065158a33448e10c
Cara0 <cara.a.crawford@gmail.com>
2018-12-10 19:07:49 -0500

    tried some ml models on the full dataset

---
+++ CC_try_some_ml_models.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
+
+df = pd.read_csv('train.csv')
+df.head()
+df.info()
+
+x = df.copy()
+x = x.drop('Cover_Type', axis = 1)
+x = x.drop('Id', axis = 1)
+
+y = df['Cover_Type']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=21)
+
+
+# specify models
+lr = LogisticRegression()
+rf = RandomForestClassifier(n_estimators=1000, max_depth=30, class_weight='balanced')
+sv = svm.SVC(kernel='l')
+
+# fit models
+lr.fit(x_train, y_train)
+rf.fit(x_train, y_train)
+sv.fit(x_train, y_train)
+
+# predict test set labels
+lr_pred = lr.predict(x_test)
+rf_pred = rf.predict(x_test)
+sv_pred = sv.predict(x_test)
+
+# evaluate model accuracies
+lr_accuracy = accuracy_score(y_test, lr_pred)
+rf_accuracy = accuracy_score(y_test, rf_pred)
+sv_accuracy = accuracy_score(y_test, sv_pred)
+
+lr_accuracy # 67%
+rf_accuracy # 86%
+sv_accuracy # 14% !!!
+
+# check distribution of y in test set...
